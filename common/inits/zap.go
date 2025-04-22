e88108b3cf812cec6ede2d7fa04ad7bf488e311f
成佳辉 <2720532440@qq.com>
2025-04-22 12:09:27 +0800

    x

--- common/inits/zap.go
+++ common/inits/zap.go
@@ -1,13 +1,19 @@
 package inits
 
-import "go.uber.org/zap"
-
-func InitZap() {
-	config := zap.NewDevelopmentConfig()
-	config.OutputPaths = []string{"D:/gocode/src/zg4/rk16/xxfz.log"}
-	build, err := config.Build()
-	if err != nil {
-		return
-	}
-	zap.ReplaceGlobals(build)
-}
+//func InitZap() {
+//	config := zap.NewDevelopmentConfig()
+//	config.OutputPaths = []string{"D:/gocode/src/zg4/rk16/project.log"}
+//	build, err := config.Build()
+//	if err != nil {
+//		return
+//	}
+//	zap.ReplaceGlobals(build)
+//
+//	xx := zap.NewDevelopmentConfig()
+//	xx.OutputPaths = []string{""}
+//	logger, err := xx.Build()
+//	if err != nil {
+//		return
+//	}
+//	zap.ReplaceGlobals(logger)
+//}
